a02788b95b46c12399abb813e303884b8168f4ef
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-19 12:07:38 -0500

    added login/signup modal

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,8 @@
 const card = $('.card-holder')
+const loginModal = $('#login-modal')
 
 card.on('click', () => console.log('clicked'))
+
+$('.fa-right-to-bracket').on('click', () => loginModal.addClass('is-active'))
+
+$('.modal-close').on('click', () => loginModal.removeClass('is-active'))
