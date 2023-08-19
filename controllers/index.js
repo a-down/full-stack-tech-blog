f3a33dbe27573e43b84ac7d84b25cb3995922d2a
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-19 11:32:27 -0500

    dashboard page created

--- controllers/index.js
+++ controllers/index.js
@@ -4,23 +4,18 @@ const { Post, User } = require('../models')
 
 router.use('/api', apiRoutes);
 
-router.use('/', async (req, res) => {
+ router.use('/dashboard', async (req, res) => {
   try {
     let postData = await Post.findAll( {include: [{model: User}]} )
     const posts = postData.map((project) => project.get({plain: true}))
     console.log(posts)
-    res.render('homepage', {posts})
+    res.render('dashboard', {posts})
+
   } catch (err) {
     res.status(500).json(err)
     console.log(err)
   }
  });
-
-router.get('/project/:id', async (req, res) => {
-  const project = await Project.findByPk(req.params.id, {include: [{model: User}]} ).catch((err) => res.status(500).json(err))
-  const serialized = project.get({plain: true})
-  res.render('project', { project: serialized })
-});
 
 router.get('/login', async (req, res) => res.render('login'))
 
@@ -29,5 +24,18 @@ router.get('/profile/:id', async (req, res) => {
   const serialized = user.get({plain: true})
   res.render('profile', {user: serialized})
 })
+
+router.use('/*', async (req, res) => {
+  try {
+    let postData = await Post.findAll( {include: [{model: User}]} )
+    const posts = postData.map((project) => project.get({plain: true}))
+    console.log(posts)
+    res.render('homepage', {posts})
+
+  } catch (err) {
+    res.status(500).json(err)
+    console.log(err)
+  }
+ });
 
 module.exports = router;
