b12048bbd2805805fa11dcee589db8dc0064681d
a-down <136649391+a2downing@users.noreply.github.com>
2023-08-20 12:51:47 -0500

    all blogpost api routes are done

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,36 +1,71 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Post } = require('../../models');
 
-router.post('/', async (req, res) => {
+
+// get all posts
+router.get('/', async (req, res) => {
+  const posts = await Post.findAll().catch((err) => res.status(500).json(err))
+  res.status(200).json(posts)
+})
+
+
+// get post by id
+router.get('/:id', async (req, res) => {
   try {
-    const newProject = await Project.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
+    const post = await Post.findByPk(req.params.id).catch((err) => res.status(500).json(err))
+    if (!post) {
+      res.status(404).json({message: 'No post found with this id!'})
+      return;
+    }
+    res.status(200).json(post)
 
-    res.status(200).json(newProject);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err)
+    console.log(err)
   }
-});
+})
 
+
+// POST new blog
+router.post('/', async (req, res) => {
+  const newPost = await Post.create(req.body).catch((err) => res.status(500).json(err))
+  res.status(200).json({message: 'Post created', newPost})
+})
+
+
+// PUT by id
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.update(req.body, {where: {id: req.params.id}})
+    if (!post) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+    res.status(200).json({message: 'Post updated'})
+  } catch (err) {
+    res.status(500).json(err)
+    console.log(err)
+  }
+})
+
+
+// DELETE by id
 router.delete('/:id', async (req, res) => {
   try {
-    const projectData = await Project.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
+    const post = await Post.destroy({
+      where: {id: req.params.id},
     });
 
-    if (!projectData) {
+    if (!post) {
       res.status(404).json({ message: 'No project found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json({message: 'Post deleted'});
+
   } catch (err) {
     res.status(500).json(err);
+    console.log(err)
   }
 });
 
